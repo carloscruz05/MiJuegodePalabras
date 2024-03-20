5a61caa5501ef06a0c890e6ffc5dd6df106d29cc
unknown <carloscruzfotografo17mail.com>
2024-03-20 16:45:27 -0600

    juego8

--- app/src/main/java/com/example/mijuegodepalabras/MainActivity2.java
+++ app/src/main/java/com/example/mijuegodepalabras/MainActivity2.java
@@ -1,39 +1,41 @@
 package com.example.mijuegodepalabras;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText editTextUsername, editTextPassword;
-    Button buttonLogin;
+
+    private static final String USUARIO_ESPECIFICO = "carlos";
+    private static final String CONTRASENIA_ESPECIFICA = "1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
+        final EditText editTextUsuario = findViewById(R.id.editTextUsuario);
+        final EditText editTextContraseña = findViewById(R.id.editTextContraseña);
+        Button buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = editTextUsername.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
+                String usuario = editTextUsuario.getText().toString();
+                String contraseña = editTextContraseña.getText().toString();
 
 
-                if (username.equals("carlos") && password.equals("1234")) {
-
+                if (usuario.equals(USUARIO_ESPECIFICO) && contraseña.equals(CONTRASENIA_ESPECIFICA)) {
                     Toast.makeText(MainActivity2.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
 
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(intent);
                 } else {
-
                     Toast.makeText(MainActivity2.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
             }
